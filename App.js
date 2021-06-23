7c6b89cb6ed0ca40c599cb331ce9dcf98b4de95e
TaeSun94 <tyzlddy@naver.com>
2021-06-23 23:47:24 +0900

    [+] Mod App.js button logic, useState in react, change plan

--- App.js
+++ App.js
@@ -3,11 +3,24 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import { Button, Divider } from "react-native-elements";
 import { LineChart, Grid, XAxis, YAxis } from "react-native-svg-charts";
+import insertComponent from "./components/insertComponent";
+function Modal(){
+  return(
+    <insertComponent />
+  )
+}
+
 export default function App() {
+
+  const [graph, setGraph] = React.useState(false);
+  const [insert, setInsert] = React.useState(false);
   const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80];
   const day = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
   ];
+  React.useEffect(()=>{
+    Modal();
+  },insert);
   const axesSvg = { fontSize: 10, fill: "grey" };
   const verticalContentInset = { top: 10, bottom: 10 };
   const xAxisHeight = 30;
@@ -20,7 +33,7 @@ export default function App() {
       </View>
       <View style={{ top: 40,height: 400, padding: 20, flexDirection: "row"}}>
         <YAxis
-          data={data}
+          data={graph ? data: day}
           style={{ marginBottom: xAxisHeight }}
           contentInset={verticalContentInset}
           svg={axesSvg}
@@ -44,11 +57,11 @@ export default function App() {
         </View>
       </View>
       <View style={{flex:2, flexDirection:'row', justifyContent:'space-around',marginTop:20}}>
-        <Button title="혈당" containerStyle={{width:100,height:40}} buttonStyle={{backgroundColor:'#fff'}} titleStyle={{color:'#000', fontSize:18}}/>
-        <Button title="혈압" containerStyle={{width:100,height:40}} buttonStyle={{backgroundColor:'#fff'}} titleStyle={{color:'#000', fontSize:18}}/>
+        <Button title="혈당" containerStyle={{width:100,height:40}} buttonStyle={{backgroundColor:'#fff'}} titleStyle={{color:'#000', fontSize:18}} onPress={()=>{setGraph(false)}}/>
+        <Button title="혈압" containerStyle={{width:100,height:40}} buttonStyle={{backgroundColor:'#fff'}} titleStyle={{color:'#000', fontSize:18}} onPress={()=>{setGraph(true)}}/>
       </View>
       <View style={{bottom:24}}>
-        <Button title={"혈당 또는 혈압 입력"} containerStyle={{borderRadius: 10, borderWidth:3, }} buttonStyle={{backgroundColor:'#fff',}} titleStyle={{color:'#000', fontSize:18}}/>
+        <Button title={"혈당 또는 혈압 입력"} containerStyle={{borderRadius: 10, borderWidth:3, }} buttonStyle={{backgroundColor:'#fff',}} titleStyle={{color:'#000', fontSize:18}} onPress={()=>{setInsert(!insert)}}/>
       </View>
     </SafeAreaView>
   );
---
+++ components/insertComponent.js
@@ -0,0 +1,14 @@
+import react from 'react';
+import {SafeAreaView} from 'react-native';
+import {} from 'react-native-elements';
+
+const insertComponent = ({}) =>{
+
+    return(
+        <SafeAreaView style={{backgroundColor:'#000'}}>
+            
+        </SafeAreaView>
+    )
+}
+
+export default insertComponent;
