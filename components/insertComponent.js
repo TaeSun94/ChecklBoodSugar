444f91450dd2983e662c20631929f371ae9a00f8
TaeSun94 <tyzlddy@naver.com>
2021-07-01 23:56:15 +0900

    [+] Mod insertComponent -> modal

--- components/insertComponent.js
+++ components/insertComponent.js
@@ -1,14 +1,40 @@
-import react from 'react';
-import {SafeAreaView} from 'react-native';
-import {} from 'react-native-elements';
+import React, { useState } from "react";
+import { SafeAreaView,Alert, Modal, StyleSheet, Text, Pressable, View } from "react-native";
+import {} from "react-native-elements";
 
-const insertComponent = ({}) =>{
-
-    return(
-        <SafeAreaView style={{backgroundColor:'#000'}}>
-            
-        </SafeAreaView>
-    )
-}
+const insertComponent = ({}) => {
+    const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <SafeAreaView style={{ backgroundColor: "#000" }}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.textStyle}>Show Modal</Text>
+      </Pressable>
+    </SafeAreaView>
+  );
+};
 
 export default insertComponent;
