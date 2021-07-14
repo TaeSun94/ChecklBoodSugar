a151eb9ea747560892f6178f7b4921b88d9a411d
TaeSun94 <tyzlddy@naver.com>
2021-07-14 23:33:35 +0900

    [+] Add Firebase library & Mod Modal in App.js

--- App.js
+++ App.js
@@ -1,39 +1,35 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Pressable,Modal } from "react-native";
 import { Button, Divider } from "react-native-elements";
+import { Input } from "react-native-elements/dist/input/Input";
 import { LineChart, Grid, XAxis, YAxis } from "react-native-svg-charts";
 import insertComponent from "./components/insertComponent";
-function Modal(){
-  return(
-    <insertComponent />
-  )
-}
 
 export default function App() {
-
   const [graph, setGraph] = React.useState(false);
   const [insert, setInsert] = React.useState(false);
   const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80];
   const day = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
   ];
-  React.useEffect(()=>{
-    Modal();
-  },insert);
   const axesSvg = { fontSize: 10, fill: "grey" };
   const verticalContentInset = { top: 10, bottom: 10 };
   const xAxisHeight = 30;
   return (
     <SafeAreaView
-      style={{ flex: 1, backgroundColor: "#fff", marginHorizontal: 14 }}
+      style={insert ? { flex: 1, backgroundColor:'#0009',paddingHorizontal: 14}:{ flex: 1, paddingHorizontal: 14 }}
     >
-      <View style={{ top:40, height:40, justifyContent:'center'}}>
-        <Text style={{fontSize:28, fontWeight:'bold', textAlign:'justify'}}>Check Health</Text>
+      <View style={{ top: 40, height: 40, justifyContent: "center" }}>
+        <Text
+          style={{ fontSize: 28, fontWeight: "bold", textAlign: "justify" }}
+        >
+          Check Health
+        </Text>
       </View>
-      <View style={{ top: 40,height: 400, padding: 20, flexDirection: "row"}}>
+      <View style={{ top: 40, height: 400, padding: 20, flexDirection: "row" }}>
         <YAxis
-          data={graph ? data: day}
+          data={graph ? data : day}
           style={{ marginBottom: xAxisHeight }}
           contentInset={verticalContentInset}
           svg={axesSvg}
@@ -56,12 +52,68 @@ export default function App() {
           />
         </View>
       </View>
-      <View style={{flex:2, flexDirection:'row', justifyContent:'space-around',marginTop:20}}>
-        <Button title="혈당" containerStyle={{width:100,height:40}} buttonStyle={{backgroundColor:'#fff'}} titleStyle={{color:'#000', fontSize:18}} onPress={()=>{setGraph(false)}}/>
-        <Button title="혈압" containerStyle={{width:100,height:40}} buttonStyle={{backgroundColor:'#fff'}} titleStyle={{color:'#000', fontSize:18}} onPress={()=>{setGraph(true)}}/>
+      <View
+        style={{
+          flex: 2,
+          flexDirection: "row",
+          justifyContent: "space-around",
+          marginTop: 20,
+        }}
+      >
+        <Button
+          title="혈당"
+          containerStyle={{ width: 100, height: 40 }}
+          buttonStyle={!insert?{ backgroundColor: "#fff" }:{backgroundColor: "#fff0" }}
+          titleStyle={{ color: "#000", fontSize: 18 }}
+          onPress={() => {
+            setGraph(false);
+          }}
+          activeOpacity={0.8}
+        />
+        <Button
+          title="혈압"
+          containerStyle={{ width: 100, height: 40 }}
+          buttonStyle={!insert?{ backgroundColor: "#fff" }:{backgroundColor: "#fff0" }}
+          titleStyle={{ color: "#000", fontSize: 18 }}
+          onPress={() => {
+            setGraph(true);
+          }}
+        />
       </View>
-      <View style={{bottom:24}}>
-        <Button title={"혈당 또는 혈압 입력"} containerStyle={{borderRadius: 10, borderWidth:3, }} buttonStyle={{backgroundColor:'#fff',}} titleStyle={{color:'#000', fontSize:18}} onPress={()=>{setInsert(!insert)}}/>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={insert}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setInsert(!insert);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <View style={{width: 300}}>
+              <Input label={"혈당"} title={"혈당"} titleStyle={styles.modalText} keyboardType={'decimal-pad'}/>
+              <Input label={"혈압"} title={"혈압"} titleStyle={styles.modalText} keyboardType={'decimal-pad'}/>
+            </View>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setInsert(!insert)}
+            >
+              <Text style={styles.textStyle}>등록 하기</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <View style={{ bottom: 24 }}>
+        <Button
+          title={"혈당 또는 혈압 입력"}
+          containerStyle={{ borderRadius: 10, borderWidth: 3 }}
+          buttonStyle={!insert?{ backgroundColor: "#fff" }:{backgroundColor: "#fff0" }}
+          titleStyle={{ color: "#000", fontSize: 18 }}
+          onPress={() => {
+            setInsert(!insert);
+          }}
+        />
       </View>
     </SafeAreaView>
   );
@@ -70,8 +122,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 3,
-    //backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
+  centeredView: {
+    flex: 1,
+    width: '100%',
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
 });
